2ca8bb0a3f8d8764cc43a1237159a635f42bd96b
Wilda <wildaakmalia14@gmail.com>
2023-07-31 10:28:11 +0700

    Initial Commit

---
+++ Project62/Project62App.swift
@@ -0,0 +1,17 @@
+//
+//  Project62App.swift
+//  Project62
+//
+//  Created by Wilda Akmalia on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Project62App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
